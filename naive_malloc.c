99aacddbdf7e2dcdbbfed08f327b0c47e0b07f9e
tassavarat <tassavarat@gmail.com>
2020-07-27 23:39:51 -0700

    Attempting extend

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,5 +1,16 @@
 #include "malloc.h"
 
+void *find_unused(char *start_addr, size_t call_nb, size_t *excess_mem)
+{
+	while (call_nb > 0)
+	{
+		*excess_mem -= *(size_t *)start_addr;
+		start_addr += *(size_t *)start_addr;
+		--call_nb;
+	}
+	return (start_addr);
+}
+
 /**
  * malloc - naive version of malloc: dynamically allocates memory on the heap
  * @size: number of bytes to allocate
@@ -11,45 +22,65 @@ void *naive_malloc(size_t size)
 	void *prev_brk;
 	static void *start_addr;
 	static size_t call_nb;
+	size_t excess_mem;
 
+	/* printf("call_nb: %lu\n", call_nb); */
+	/* printf("size req: %lu\n", size); */
+	excess_mem = sysconf(_SC_PAGESIZE);
 	if (!start_addr)
 	{
-		printf("page_size: %li\n", sysconf(_SC_PAGESIZE));
-		printf("size req: %lu\n", size);
-		printf("alinged req: %lu\n", align(size));
+		/* printf("alinged req: %lu\n", align(size)); */
 		prev_brk = sbrk(sysconf(_SC_PAGESIZE));
 		if (prev_brk == (void *) -1)
 			return (NULL);
 		start_addr = prev_brk;
-		printf("start_addr: %p\n", start_addr);
 	}
+	else
+	{
+		prev_brk = find_unused(start_addr, call_nb, &excess_mem);
+	}
+	excess_mem -= size + sizeof(size_t);
+	printf("excess_mem: %lu\n", excess_mem);
+	if (excess_mem - size + sizeof(size_t) < size + sizeof(size_t))
+	{
+		/* puts("CALLED"); */
+		excess_mem = sysconf(_SC_PAGESIZE);
+		prev_brk = sbrk(sysconf(_SC_PAGESIZE));
+		if (prev_brk == (void *) -1)
+			return (NULL);
+	}
+	/* *(size_t *)prev_brk = align(size) + sizeof(size_t); */
+	*(size_t *)prev_brk = size + sizeof(size_t);
+	/* excess_mem -= *(size_t *)prev_brk; */
+	/* printf("excess_mem: %lu\n", excess_mem); */
+	/* printf("target: %lu\n", *(size_t *)prev_brk); */
+	*(size_t *)((char *)prev_brk + *(size_t *)prev_brk) = excess_mem;
 	++call_nb;
-	printf("call_nb: %lu\n", call_nb);
-	*(size_t *)prev_brk = align(size) + sizeof(size_t);
 	return ((char *)prev_brk + sizeof(size_t));
 }
 
 int main(void)
 {
-	char *str;
-	int i;
+    char *str;
+    int i;
 
-	printf("Starting break is %p\n", sbrk(0));
+    printf("Starting break is %p\n", sbrk(0));
 
-	for (i = 0; i < 10; i++)
-	{
-		void *chunk;
+    for (i = 0; i < 10; i++)
+    {
+        void *chunk;
 
-		str = naive_malloc(10);
-		strcpy(str, "Holberton");
-		str[21] = '\0';
-		printf("%p: %s, ", (void *)str, str);
-		chunk = (void *)(str - sizeof(size_t));
-		printf("chunk addr: %p, ", (void *)chunk);
-		printf("size: %lu, ", *((size_t *)chunk));
-		printf("break: %p\n", sbrk(0));
-	}
+        /* str = naive_malloc(10); */
+        str = naive_malloc(1016);
+        strcpy(str, "Holberton");
+        str[21] = '\0';
+        printf("%p: %s, ", (void *)str, str);
+        chunk = (void *)(str - sizeof(size_t));
+        printf("chunk addr: %p, ", (void *)chunk);
+        printf("size: %lu, ", *((size_t *)chunk));
+        printf("break: %p\n", sbrk(0));
+    }
 
-	printf("Final break is %p\n", sbrk(0));
-	return (EXIT_SUCCESS);
+    printf("Final break is %p\n", sbrk(0));
+    return (EXIT_SUCCESS);
 }
