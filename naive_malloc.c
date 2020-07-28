81aaf2b5328ce9b6811e1bdb18f04ce3d7b58d32
tassavarat <tassavarat@gmail.com>
2020-07-28 02:17:12 -0700

    Working on extend

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,12 +1,9 @@
 #include "malloc.h"
 
-void *find_unused(char *start_addr, size_t call_nb, size_t *excess_mem)
+void *find_unused(char *start_addr, size_t call_nb)
 {
 	while (call_nb > 0)
 	{
-		*excess_mem -= *(size_t *)start_addr;
-		if (*excess_mem == 0)
-			*excess_mem = sysconf(_SC_PAGESIZE);
 		start_addr += *(size_t *)start_addr;
 		--call_nb;
 	}
@@ -24,38 +21,38 @@ void *naive_malloc(size_t size)
 	void *prev_brk;
 	static void *start_addr;
 	static size_t call_nb;
-	size_t excess_mem;
+	size_t chunk_size, excess_mem;
 
 	/* printf("call_nb: %lu\n", call_nb); */
 	/* printf("size req: %lu\n", size); */
-	excess_mem = sysconf(_SC_PAGESIZE);
+	chunk_size = size + sizeof(size_t);
 	if (!start_addr)
 	{
-		/* printf("alinged req: %lu\n", align(size)); */
-		prev_brk = sbrk(sysconf(_SC_PAGESIZE));
+		puts("SBRK");
+		prev_brk = sbrk(_SC_PAGESIZE);
 		if (prev_brk == (void *) -1)
 			return (NULL);
 		start_addr = prev_brk;
+		excess_mem = sysconf(_SC_PAGESIZE);
 	}
 	else
 	{
-		prev_brk = find_unused(start_addr, call_nb, &excess_mem);
+		prev_brk = find_unused(start_addr, call_nb);
+		excess_mem = *(size_t *)prev_brk;
 	}
-	excess_mem -= size + sizeof(size_t);
-	printf("excess_mem: %lu\n", excess_mem);
-	/* if (excess_mem - size + sizeof(size_t) < size + sizeof(size_t)) */
-	if (excess_mem < size + sizeof(size_t))
+	if (excess_mem < chunk_size + sizeof(size_t))
 	{
-		puts("CALLED");
-		excess_mem = sysconf(_SC_PAGESIZE) - (size + sizeof(size_t));
-		prev_brk = sbrk(sysconf(_SC_PAGESIZE));
-		if (prev_brk == (void *) -1)
+		puts("SBRK");
+		if (sbrk(_SC_PAGESIZE) == (void *) -1)
 			return (NULL);
+		excess_mem += sysconf(_SC_PAGESIZE);
 	}
-	/* *(size_t *)prev_brk = align(size) + sizeof(size_t); */
-	*(size_t *)prev_brk = size + sizeof(size_t);
-	/* excess_mem -= *(size_t *)prev_brk; */
+	excess_mem -= chunk_size;
 	printf("excess_mem: %lu\n", excess_mem);
+	/* *(size_t *)prev_brk = align(size) + sizeof(size_t); */
+	*(size_t *)prev_brk = chunk_size;
+	/* excess_mem -= *(size_t *)prev_brk; */
+	/* printf("excess_mem: %lu\n", excess_mem); */
 	/* printf("target: %lu\n", *(size_t *)prev_brk); */
 	*(size_t *)((char *)prev_brk + *(size_t *)prev_brk) = excess_mem;
 	++call_nb;
