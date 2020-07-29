13fba61ef764e457f793b310d45120616a0e64d4
Farrukh <farruh1996@gmail.com>
2020-07-28 23:34:34 -0700

    Allocate memory randomly

--- 1-main.c
+++ 1-main.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include "malloc.h"
 
 void *_malloc(size_t size);
 
+#define LOWER 10
+#define UPPER 10000
 /**
  * main - Program entry point
  *
@@ -18,19 +21,12 @@ int main(void)
 
 	setbuf(stdout, NULL);
 	printf("Starting break is %p\n", sbrk(0));
-
-	for (i = 0; i < 10; i++)
+	srand(time(0));
+	for (i = 0; i < 100; i++)
 	{
 		void *chunk;
 
-		str = _malloc(1024);
-		strcpy(str, "Holberton");
-		str[21] = '\0';
-		printf("%p: %s, ", (void *)str, str);
-		chunk = (void *)(str - sizeof(size_t));
-		printf("chunk addr: %p, ", (void *)chunk);
-		printf("size: %lu, ", *((size_t *)chunk));
-		str = _malloc(278);
+		str = _malloc(LOWER + (rand() % (UPPER - LOWER + 1)));
 		strcpy(str, "Holberton");
 		str[21] = '\0';
 		printf("%p: %s, ", (void *)str, str);
